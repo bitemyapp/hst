24ee01f5cfe60a0c561e7e9633f108e9cf463341
levi.stephen <levi.stephen@845cee7f-5637-0410-8c2c-534d55b28bf2>
2007-08-10 12:51:30 +0000

    Initial Revision

---
+++ src/HST/Execute.hs
@@ -0,0 +1,34 @@
+{-
+   Copyright 2007 Levi Stephen
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-}
+
+module HST.Execute where
+
+import HST.Parse
+
+data Object = TranscriptObject | StringObject String
+    deriving Show
+
+evaluatePrimary :: Primary -> Object
+evaluatePrimary (PrimaryLiteral (StringLiteral s)) = StringObject s
+evaluatePrimary (PrimaryIdentifier (Identifier "Transcript")) = TranscriptObject
+
+prepareMessage (KeywordMessage (Keyword k) p) = (k, [evaluatePrimary p])
+
+evaluateExpression (BasicExpression prim msg) = sendMessage (evaluatePrimary prim) sel args
+    where (sel, args) = prepareMessage msg
+
+sendMessage TranscriptObject "show" [(StringObject s)] = putStrLn s
+
